383c394a37e745d4da218d9baf0c43f77f087885
beckheng <beckheng@gmail.com>
2014-06-25 18:15:10 +0800

    android 版本

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -8,17 +8,25 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/cocos2dx/util.cpp \
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/MainMenuLayer.cpp \
+                   ../../Classes/PlayScene.cpp \
+                   ../../Classes/ScoreScene.cpp \
+                   ../../Classes/util/util.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
-
+LOCAL_WHOLE_STATIC_LIBRARIES += spritebuilder_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,2d)
 $(call import-module,audio/android)
 $(call import-module,Box2D)
+$(call import-module,editor-support/spritebuilder)
+
