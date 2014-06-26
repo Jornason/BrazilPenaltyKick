7a8e12ca245d8693448af246c22b2f2f71047b6b
beckheng <beckheng@gmail.com>
2014-06-26 12:25:07 +0800

    android build.

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -1,6 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libsqlite
+LOCAL_SRC_FILES := ../thirdparty/libsqlite.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -9,19 +14,23 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
                    ../../Classes/cocos2dx/util.cpp \
+                   ../../Classes/db/SqliteHandler.cpp \
+                   ../../Classes/global.cpp \
                    ../../Classes/HelloWorldScene.cpp \
                    ../../Classes/MainMenuLayer.cpp \
                    ../../Classes/PlayScene.cpp \
                    ../../Classes/ScoreScene.cpp \
                    ../../Classes/util/util.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+					/Users/beckheng/Downloads/sqlite-amalgamation-3080301
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 LOCAL_WHOLE_STATIC_LIBRARIES += spritebuilder_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += libsqlite
 
 include $(BUILD_SHARED_LIBRARY)
 
